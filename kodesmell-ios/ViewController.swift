241e6ccf4ae147b498e5a106218c001a6b346084
sangcomz <sangcomz@naver.com>
2017-08-13 23:51:24 +0900

    Initial Commit

---
+++ kodesmell-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kodesmell-ios
+//
+//  Created by JeongSeokWon on 13/08/2017.
+//  Copyright © 2017 Kodesmell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
